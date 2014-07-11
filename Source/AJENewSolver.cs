113436e9149403016772505110886eb4c4512b56
ferram4 <ferram40@gmail.com>
2014-07-11 01:23:07 -0400

    Add turbofan variables, area-mach relation

--- Source/AJENewSolver.cs
+++ Source/AJENewSolver.cs
@@ -34,8 +34,11 @@ namespace AJE
         //Diffuser effects; tau_d = 1, pi_d slightly less than 1
         private double pi_d, tau_d;
 
-        //Compressor effects; pi_c is defined, tau_c is directly related to pi_c, eta_c is an efficiency used to relate pi_c to tau_c
+        //Compressor effects; pi_c is by the power from the turbine or is limited by design, tau_c is directly related to pi_c, eta_c is an efficiency used to relate pi_c to tau_c
         private double pi_c, tau_c, eta_c;
+
+        //Other Compressor; this is the maximum pressure ratio the compressor is capable of
+        private double pi_c_max;
 
         //Burner effects; pi_b is slightly less than 1, tau_b > 1
         private double pi_b, tau_b;
@@ -55,6 +58,11 @@ namespace AJE
         //Nozzle effects; pi_n is slightly less than 1, tau_n = 1
         private double pi_n, tau_n;
 
+        //Fan effects; pi_fc is limited by design or is proportional to pi_c; tau_fc is related to pi_fc by eta_fc
+        private double pi_fc, tau_fc, eta_fc;
+
+        //Other fan; maximum pressure ratio of fan
+        private double pi_fc_max;
 
         //Important flow variables
         //These are necessary for calculating the thrust and efficiency of the engine
@@ -68,8 +76,14 @@ namespace AJE
         //The pressure, temperature, mach number, sound speed and velocity of the freestream; used to calculate initial properties for engine flow and for thrust
         private double P0, T0, M0, a0, U0;
 
-        //The area sucked into the engine at the freestream, the area of the compressor inlet, area of the nozzle throat and the exit area of the engine
+        //The area sucked into the core engine at the freestream, the area of the compressor inlet, area of the nozzle throat and the exit area of the engine
         private double A0, A2, A8, A9;
+
+        //The ratio of air going through the fan rather than through the core
+        private double bypassRatio;
+
+        //The pressure, temperature, mach number, sound speed and velocity at the exit of the fan nozzle (station 9f); used to calculate thrust
+        private double P9f, T9f, M9f, a9f, U9f;
 
         //Fraction fuel flow for burner and afterburner respectively; used for both exit mass flow and for fuel consumption
         private double f, f_ab;
@@ -99,7 +113,7 @@ namespace AJE
         private double a_9_rat_astar_8_min, a_9_rat_astar_8_max, a_9_rat_astar_8_actual;
 
         //Must be first initialization function called
-        public void InitializeEngineGeometry(double compressorInletArea, double burnerExitArea, double nozzleThroatArea, double nozzleMinExitArea, double nozzleMaxExitArea)
+        public void InitializeEngineGeometry(double compressorInletArea, double burnerExitArea, double nozzleThroatArea, double nozzleMinExitArea, double nozzleMaxExitArea, double bypass)
         {
             astar_4_rat_astar_8 = burnerExitArea / nozzleThroatArea;
             a_2_rat_astar_4 = compressorInletArea / burnerExitArea;
@@ -107,13 +121,17 @@ namespace AJE
             a_9_rat_astar_8_max = nozzleMaxExitArea / nozzleThroatArea;
             A8 = nozzleThroatArea;
             A2 = compressorInletArea;
+            bypassRatio = bypass;
         }
 
         //Must be called after InitializeEngineGeometry
-        public void InitializeComponentEfficiencies(double diffuserPresRatio, double compressorEfficiency, double burnerPresRatio, double turbineEfficiency, double nozzlePresRatio)
+        public void InitializeComponentEfficiencies(double diffuserPresRatio, double compressorEfficiency, double maxCompressorPressureRatio, double fanEfficiency, double maxFanPressureRatio, double burnerPresRatio, double turbineEfficiency, double nozzlePresRatio)
         {
             pi_d = diffuserPresRatio;
             eta_c = compressorEfficiency;
+            pi_c_max = maxCompressorPressureRatio;
+            eta_fc = fanEfficiency;
+            pi_fc_max = maxFanPressureRatio;
             pi_b = burnerPresRatio;
             eta_t = turbineEfficiency;
             pi_n = nozzlePresRatio;
@@ -156,7 +174,7 @@ namespace AJE
             CalculateRamProperties();
             CalculateTurbinePropertiesFromNozzleMassBalancing();
             CalculateBurnerProperties();
-            CalculateCompressorPropertiesFromTurbineEnergyBalance();
+            CalculateCompressorAndFanPropertiesFromTurbineEnergyBalance();
             CalculateAfterburnerProperties();
 
             CalculateSuctionAreaForMassFlow();
@@ -168,10 +186,21 @@ namespace AJE
             a9 = Math.Sqrt(gamma_ab * R_ab * T9);
             U9 = M9 * a9;
 
-            //Effects of velocity difference at inlet and nozzle
-            thrust = mdot * ((1 + f + f_ab) * U9 - U0);
-            //Effects of pressure difference at inlet and nozzle
+            a9f = Math.Sqrt(gamma_c * R_c * T9f);
+            U9f = M9f * a9f;
+
+            //Effects of velocity difference at inlet and nozzle of core
+            thrust = ((1 + f + f_ab) * U9 - U0);
+
+            //Effects of velocity difference across fan
+            thrust += (U9f - U0) * bypassRatio;
+            thrust *= mdot;
+
+            //Effects of pressure difference at inlet and nozzle of core
             thrust += (P9 - P0) * A9;
+
+            //Effects of pressure difference across fan
+            thrust += (P9f - P0) * A9 * bypassRatio;
 
             I_sp = mdot * (f + f_ab) * g_0;
             I_sp = thrust / I_sp;
@@ -192,17 +221,24 @@ namespace AJE
             pi_r = Math.Pow(tau_r, tmp);
         }
 
-        public void CalculateCompressorPropertiesFromTurbineEnergyBalance()
+        public void CalculateCompressorAndFanPropertiesFromTurbineEnergyBalance()
         {
             tau_c = tau_t - 1;
             tau_c *= -(1 + f) * tau_lambda;
             tau_c /= tau_r;
             tau_c++;
+            tau_c /= (bypassRatio + 1);
 
-            pi_c = gamma_c - 1;
+            tau_fc = tau_c * bypassRatio;
+
+            pi_c = pi_fc = gamma_c - 1;
             pi_c *= eta_c;
             pi_c = gamma_c / pi_c;
             pi_c = Math.Pow(tau_c, pi_c);
+
+            pi_fc *= eta_fc;
+            pi_fc = gamma_c / pi_fc;
+            pi_fc = Math.Pow(tau_fc, pi_fc);
         }
 
         public void CalculateBurnerProperties()
@@ -245,7 +281,7 @@ namespace AJE
         {
             CalculateCompressorLineFromCompressorTurbineMassBalance();
 
-            A0 = A2 * astar_2_rat_a_2 / CalculateAreaRatioFromMachNumber(M0);
+            A0 = A2 * astar_2_rat_a_2 / CalculateAreaRatioFromMachNumber(M0, gamma_c);
         }
 
         public void CalculateMassFlow(double air_density)
@@ -266,24 +302,32 @@ namespace AJE
 
         public void CalculateExitMachNumberAndExitArea()
         {
-            M9 = CalculateMachNumberFromAreaRatioSupersonic(a_9_rat_astar_8_actual);
+            M9 = CalculateMachNumberFromAreaRatioSupersonic(a_9_rat_astar_8_actual, gamma_ab);
             A9 = A8 * a_9_rat_astar_8_actual;
+            M9f = 0.5;
         }
 
         public void CalculateExitTemperature()
         {
-            //total temperature at exit
+            //total temperature at exit of the core
             double Tt9 = T0 * tau_lambda_ab * Cp_c / Cp_ab;
 
             T9 = M9 * M9;
             T9 *= (gamma_ab - 1) * 0.5;
             T9++;
             T9 = Tt9 / T9;
+
+            //total temp at exit of the fan
+            Tt9 = T0 * tau_fc;
+            T9f = M9f * M9f;
+            T9f *= (gamma_c - 1) * 0.5;
+            T9f++;
+            T9f = Tt9 / T9f;
         }
 
         public void CalculateExitPressure()
         {
-            //total temperature at exit
+            //total pressure at exit of the core
             double Pt9 = T0 * pi_r * pi_c * pi_b * pi_t * pi_ab * pi_n;
 
             P9 = M9 * M9;
@@ -291,6 +335,14 @@ namespace AJE
             P9++;
             P9 = Math.Pow(P9, gamma_ab / (gamma_ab - 1));
             P9 = Pt9 / P9;
+
+            //total pressure at exit of fan
+            Pt9 = T0 * pi_r * pi_fc;
+            P9f = M9f * M9f;
+            P9f *= (gamma_c - 1) * 0.5;
+            P9f++;
+            P9f = Math.Pow(P9f, gamma_c / (gamma_c - 1));
+            P9f = Pt9 / P9f;
         }
 
         public double CalculateGamma(double temperature, double fuel_fraction)
@@ -308,19 +360,79 @@ namespace AJE
             return 287;
         }
 
-        public double CalculateAreaRatioFromMachNumber(double mach)
+        //Calculates the area ratio necessary for sonic flow directly from mach number
+        public double CalculateAreaRatioFromMachNumber(double mach, double gamma)
         {
-            return 0;
+            double gammaPower = gamma - 1;
+            gammaPower *= 2;
+            gammaPower = (gamma + 1) / gammaPower;
+
+            return CalculateAreaRatioFromMachNumber(mach, gamma, gammaPower);
         }
 
-        public double CalculateMachNumberFromAreaRatioSupersonic(double areaRatio)
+        public double CalculateAreaRatioFromMachNumber(double mach, double gamma, double gammaPower)
         {
-            return 0;
+            double result = gamma - 1;
+            result *= 0.5;
+
+            result *= mach * mach;
+            result++;
+
+            result *= 2;
+            result /= (gamma + 1);
+
+            result = Math.Pow(result, gammaPower);
+            result /= mach;
+            return result;
         }
 
-        public double CalculateMachNumberFromAreaRatioSubsonic(double areaRatio)
+        public double CalculateMachNumberFromAreaRatioSupersonic(double areaRatio, double gamma)
         {
-            return 0;
+            return CalculateMachNumberFromAreaRatio(areaRatio, gamma, 2);
+        }
+
+
+        public double CalculateMachNumberFromAreaRatio(double areaRatio, double gamma, double guess)
+        {
+            double numeratorConstant = 1 - 3 * gamma;
+            numeratorConstant /= (2 - 2 * gamma);
+            numeratorConstant = Math.Pow(2, numeratorConstant);
+
+            double gammaPower = gamma - 1;
+            gammaPower *= 2;
+            gammaPower = (gamma + 1) / gammaPower;
+
+            double mach_last = 0;
+            double fn = 1;
+            double fn_prime = 1;
+            while(Math.Abs(guess - mach_last) <= 0.01)
+            {
+                mach_last = guess;
+                fn = CalculateAreaRatioFromMachNumber(guess, gamma, gammaPower);
+
+                fn_prime = gamma - 1;
+                fn_prime *= guess * guess;
+                fn_prime += 2;
+                fn_prime *= guess * guess;
+
+                fn_prime = gammaPower / fn_prime;
+                fn_prime *= (guess * guess - 1);
+
+                double tmp = guess * guess * 0.5 * (gamma - 1);
+                tmp++;
+                tmp /= (gamma + 1);
+
+                fn_prime *= Math.Pow(tmp, gammaPower);
+
+                guess = guess - fn / fn_prime;
+            }
+
+            return guess;
+        }
+
+        public double CalculateMachNumberFromAreaRatioSubsonic(double areaRatio, double gamma)
+        {
+            return CalculateMachNumberFromAreaRatio(areaRatio, gamma, 0.5);
         }
     }
 }
